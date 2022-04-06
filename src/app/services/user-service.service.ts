640a95fe4e7236da04df1782d71d8168558c1a65
cerafinn <74612014+cerafinn@users.noreply.github.com>
2022-04-06 16:34:39 -0400

    worked on interface and service for user

---
+++ src/app/services/user-service.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, Subject } from 'rxjs';
+import { User } from '../interfaces/User';
+
+import { environment } from '../../environments/environment'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserServiceService {
+
+  http: HttpClient;
+
+  subject: Subject<User> = new Subject<User>();
+
+  constructor(http: HttpClient) {
+    this.http = http;
+  }
+
+  // need backend functionality?
+  login(username: string, password: string) {
+    //function to authenticate user
+  }
+
+  logout() {
+    // need to navigate to home page on logout
+  }
+
+  register() {
+    //function to add new user, post request
+  }
+
+  getAllUsers() {
+    return this.http.get<User[]>(`${environment.apiUrl}/users`)
+  }
+}
