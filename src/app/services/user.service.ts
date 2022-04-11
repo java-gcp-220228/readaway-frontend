5d51b960f407fa85bed7fdff83f41b58943b965c
cerafinn <74612014+cerafinn@users.noreply.github.com>
2022-04-11 18:01:13 -0400

    updates to user and auth service, login and registration components

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -2,8 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, Subject } from 'rxjs';
 import { User } from '../interfaces/User';
-
 import { environment } from '../../environments/environment'
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
@@ -13,22 +13,32 @@ export class UserService {
 
   http: HttpClient;
 
-  subject: Subject<User> = new Subject<User>();
+  registerErrorSubject: Subject<string> = new Subject<string>();
 
-  constructor(http: HttpClient) {
-    this.http = http;
-  }
+  constructor(http: HttpClient, private router: Router) {  }
 
   logout() {
+    localStorage.removeItem('jwt');
+    this.router.navigate([' ']);
   }
 
-  register() {
-    // first check if username is available, then register user
+  register(email: string, username: string, password: string) {
+    this.http.post<User>(`${environment.apiUrl}/register`, { email: email, username: username, password: password},
+    {
+      'observe': 'response',
+      'headers': { Authorization: `Bearer ${localStorage.getItem('jwt')}` }
+    })
+    .subscribe(res => {
+      const jwt = res.headers.get('token');
+      localStorage.setItem('jwt', jwt);
 
-    //fetch
-
-    //post to register user
-    // post to login
+      localStorage.setItem('user_info', JSON.stringify(res.body));
+      console.log(sessionStorage.getItem('user_info'));
+      this.router.navigate(['profile']);
+    }, err => {
+      const errorMessage = err.error;
+      this.registerErrorSubject.next(errorMessage);
+    });
   }
 
   getAllUsers() {
