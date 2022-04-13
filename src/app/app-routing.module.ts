b7f255985c9b284832109d88614125ae65178707
Caitlin Talerico <cetalerico@hotmail.com>
2022-04-12 20:38:07 -0600

    Adjust footer size and align icons. Add styling to login page and about page.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,14 +4,17 @@ import { LoginComponent } from './components/login/login.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 import { GiveawayDisplayComponent } from './components/giveaway-display/giveaway-display.component';
 import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent},
+  { path: 'about', component: AboutComponent},
   //{ path: 'giveaways', component: GiveawayTableComponent}
+  { path: "login", component: LoginComponent},
   { path: 'giveaways/:id', component: GiveawayDisplayComponent},
   { path: '', redirectTo: '/home', pathMatch: 'full'},
   { path: '**', component: PageNotFoundComponent},
-  { path: "login", component: LoginComponent},
+
 //  { path: 'login', redirectTo: "/login", pathMatch: "full" }
   
 ];
